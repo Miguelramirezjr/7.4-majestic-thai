e64253bfb7db27c8d27f696da0f64f0c5580245b
Jacob Smith <jacob@jacobsmith.io>
2015-10-07 11:55:13 -0400

    Make create form work

--- app/scripts/views/products/create.js
+++ app/scripts/views/products/create.js
@@ -1,9 +1,27 @@
 var CreateProductView = Backbone.View.extend({
   template: JST['products/create'],
+
+  events: {
+    'submit form': 'createProduct'
+  },
+
   render: function() {
     this.$el.html(this.template());
     return this;
+  },
+
+  createProduct: function(e) {
+    e.preventDefault();
+    var title = this.$('input[name=title]').val();
+    var price = Number(this.$('input[name=price]').val());
+    this.collection.create({
+      title: title,
+      price: price
+    });
+
+    this.$('input').not('[type=submit]').val('');
   }
+
 });
 
 export default CreateProductView;
