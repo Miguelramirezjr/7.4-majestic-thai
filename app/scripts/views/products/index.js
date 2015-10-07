6c0cad292278295e0e5ede71abf76c43e0124b7a
Jacob Smith <jacob@jacobsmith.io>
2015-10-07 10:02:19 -0400

    Implement naive addChild

--- app/scripts/views/products/index.js
+++ app/scripts/views/products/index.js
@@ -1,13 +1,17 @@
+import ProductItemView from 'views/products/item';
+
 var ProductsIndexView = Backbone.View.extend({
-  template: JST['products/index'],
+  tagName: 'ul',
 
   initialize: function() {
-    this.listenTo(this.collection, 'sync reset', this.render);
+    this.listenTo(this.collection, 'add', this.addChild);
+    this.listenTo(this.collection, 'remove', this.removeChild);
+    this.listenTo(this.collection, 'sort', this.sortChildren);
   },
 
-  render: function() {
-    this.$el.html(this.template(this.collection.toJSON()));
-    return this;
+  addChild: function(post, collection) {
+    var view = new ProductItemView({model: post});
+    this.$el.append(view.render().el);
   }
 });
 
---
+++ app/scripts/views/products/item.js
@@ -0,0 +1,14 @@
+var ProductItemView = Backbone.View.extend({
+  
+  tagName: 'li',
+
+  template: JST['products/item'],
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+
+});
+
+export default ProductItemView;
